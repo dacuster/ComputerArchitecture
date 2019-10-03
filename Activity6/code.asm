65ab7c0c5b447cecb936a5c51c664649223fe8ac
Nick <dacosta.nick@yahoo.com>
2019-10-03 13:42:54 -0400

    Updated activity 6
    Implemented actual activity 6 code. Appendix E

--- Activity6/code.asm
+++ Activity6/code.asm
@@ -1,43 +1,14 @@
-; Program 6.1
-; Sum Program - MASM (32-bit)
-; Copyright (c) 2017 Hall & Slonka
-
+; code.asm
 .386
-.MODEL FLAT, C
-ExitProcess PROTO stdcall, dwExitCode:DWORD
+.MODEL FLAT, stdcall
+.STACK 4096
+.CODE
+asmMain PROC C
+	int	3		; system interupt to debug in obj files
+	xor eax, eax
+	mov eax, 0
+	inc eax
 
-.data
-num1 DWORD 2
-num2 DWORD 4
-
-.code
-_main PROC
-
-mov eax, 10
-dec eax
-mov ebx, 5
-
-push num2
-push num1
-call _sum
-add esp, 8 ; Add 8 bytes to esp to increase stack size to dereference 2 parameters passed to function.
-
-add eax, ebx
-dec eax
-
-INVOKE ExitProcess, 0
-_main ENDP
-
-_sum PROC
-push ebp;Save caller base frame pointer
-mov ebp, esp ; push the current memory location(ebp) of this function
-push ebx	; local variable (just example. not needed)
-mov ebx, [ebp + 8]	; access first variable passed to function. jump past 2 memory location (previous functions stack location and return type location)
-mov eax, [ebp + 12]
-add eax, ebx ; eax is guaranteed to save when going back to caller function.
-pop ebx ; put the value of ebx back from the stack
-pop ebp ; point back to the caller stack pointer
-ret
-_sum ENDP
-
+	ret
+asmMain ENDP
 END
