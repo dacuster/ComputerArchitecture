7ca126eccb3102e2e0339099ae4bf8aec7251fb5
Nick <dacosta.nick@yahoo.com>
2019-11-07 09:58:02 -0500

    Added SSE operations

--- Assignment_9-1/Assignment_9-1/main.cpp
+++ Assignment_9-1/Assignment_9-1/main.cpp
@@ -13,43 +13,57 @@
 
 int main()
 {
+	float PI;
 	float userInput1 = 0.0f;
 	float userInput2 = 0.0f;
 	float result1 = 0.0f;
 	float result2 = 0.0f;
 	float result3 = 0.0f;
 
+	// Ask for user input.
 	std::cout << "Enter a floating point number: ";
 	std::cin >> userInput1;
-	std::cout << std::endl << "Enter a second floating point number: ";
+	std::cout << "Enter a second floating point number: ";
 	std::cin >> userInput2;
 
-	// x87 floating-point.
+	// Title for FPU operations.
+	std::cout << std::endl << "FPU Instructions" << std::endl;
+	// x87 Floating-Point.
 	__asm
 	{
 		finit
 		fldpi // Load PI into the FPU stack.
+		fst PI // Set the PI variable to PI.
 		fmul userInput1 // Multiply userInput1 by the value on the top of the FPU stack.
 		fsub userInput2 //  Subtract the value on the top of the FPU stack by userInput2.
 		fstp result1 // Pop the top of the FPU stack and place the value in result1.
 	};
-
+	// Print the result from the FPU operations.
 	std::cout << "PI * " << userInput1 << " - " << userInput2 << " = " << result1 << std::endl;
 
+	// Title for SSE operations.
+	std::cout << std::endl << "SSE Instructions" << std::endl;
 	// SSE Scalar.
 	__asm
 	{
-		movss xmm0, userInput2
-		addss xmm0, userInput1
-		movss userInput1, xmm0
+		movss xmm0, PI // Move PI to xmm0.
+		mulss xmm0, userInput1 // Multiply userInput1 by xmm0(PI) and store it in xmm0.
+		subss xmm0, userInput2 // Subtract userInput2 from the value in xmm0 and store in xmm0.
+		movss result2, xmm0 // Move the value in xmm0 to result2.
 	};
 
+	// Print the result from the SSE operations.
+	std::cout << "PI * " << userInput1 << " - " << userInput2 << " = " << result2 << std::endl;
+
+	// Title for AVX operations.
+	std::cout << std::endl << "AVX Instructions" << std::endl;
 	__asm
 	{
 
 	};
 
-	std::cout << userInput1 << std::endl;
+	// Print the result from the AVX operations.
+	std::cout << result3 << std::endl;
 
 	return 0;
 }
