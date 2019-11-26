d737518a71503a289aee23b4081e0f86f02c78de
Nick <dacosta.nick@yahoo.com>
2019-11-26 01:22:31 -0500

    Final project files and others

---
+++ Project1/Source.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+{
+	int test = 0;
+
+
+	if (test == 1)
+	{
+		std::cout << "One";
+	}
+	else if (test == 2)
+	{
+		std::cout << "Two";
+	}
+	else
+	{
+		std::cout << "Other";
+	}
+
+	std::cin;
+
+	return 0;
+}
